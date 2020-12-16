7117c4b00abbacb2628fb5c1848841ab3c8b3462
Saachee <72810211+Saachee@users.noreply.github.com>
2020-12-16 08:32:27 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project-27
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ paper.js
@@ -0,0 +1,35 @@
+class paper {
+    constructor(x,y,r){
+        
+    
+    var options={
+        isStatic:false,
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
+
+
+    }
+    this.x=x;
+    this.y=y;
+    this.r=r;
+    this.image=loadImage("paper.png");
+    this.body=Bodies.circle(this.x, this.y,(this.r-20)/2, options)
+    World.add(world, this.body);
+}
+ 
+display(){
+
+    var paperPos=this.body.position;
+
+    push()
+    translate(paperPos.x,paperPos.y+10);
+    rectMode(CENTER)
+    fill(255,0,255)
+    imageMode(CENTER)
+    image(this.image,0,this.r,this.r);
+    pop();
+
+}
+
+}
